7e74a1a5eee83293394bf8b0c14a1c4678a2a104
sbejaoui <souheil.bejaoui@acsone.eu>
2023-02-22 10:36:05 +0100

    [IMP] - add backend options as property field

--- shipment_advice_planner_toursolver/models/toursolver_backend.py
+++ shipment_advice_planner_toursolver/models/toursolver_backend.py
@@ -3,8 +3,10 @@
 
 from datetime import datetime, timedelta
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+
+from .tools import seconds_to_duration
 
 
 class TourSolverBackend(models.Model):
@@ -19,8 +21,14 @@ class TourSolverBackend(models.Model):
     url = fields.Char()
     api_key = fields.Char()
     delivery_window_disabled = fields.Boolean()
-    delivery_window_start = fields.Float(default=8.0)
-    delivery_window_end = fields.Float(default=17.0)
+    partner_defaul_delivery_window_start = fields.Float(
+        default=8.0,
+        help="If no delivery winodow specified on the partner this will be used",
+    )
+    partner_default_delivery_window_end = fields.Float(
+        default=17.0,
+        help="If no delivery winodow specified on the partner this will be used",
+    )
     delivery_duration = fields.Integer(
         string="Fixed time spent delivering a customer",
         help="Duration in seconds needed to deliver a customer",
@@ -33,13 +41,31 @@ class TourSolverBackend(models.Model):
     loading_duration = fields.Integer(
         string="Fixed initial loading time", help="Loading time in minutes"
     )
-    work_penalty = fields.Float(
-        "Fixed cost working/hour", help="The cost of a resource working for an hour."
+    resource_default_work_penalty = fields.Float(
+        string="Resource fixed cost working/hour",
+        help="Default value for the cost of a resource working for an hour. "
+        "Can be specified on resource level using `travelPenalty`option",
     )
-    travel_penalty = fields.Float(
-        "Fixed cost travelling/hour",
-        help="The cost for a resource of driving for one distance unit.",
+    resource_default_travel_penalty = fields.Float(
+        string="Resource fixed cost travelling/hour",
+        help="Default value for the The cost for a resource of driving for one distance"
+        " unit. Can be specified on resource level using `workPenalty`option",
     )
+    definition_id = fields.Many2one(
+        comodel_name="toursolver.backend.option.definition", readonly=True
+    )
+    backend_options = fields.Properties(
+        string="Options",
+        copy=True,
+        definition="definition_id.backend_options_definition",
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for rec in records:
+            rec.definition_id = rec.definition_id.create({})
+        return records
 
     def _get_partner_delivery_duration(self, partner):
         self.ensure_one()
@@ -51,7 +77,7 @@ class TourSolverBackend(models.Model):
 
     def _get_work_start_time(self):
         """
-        Return the start time of the delivery rounds to geo-optimize.
+        Return the start time of the delivery to geo-optimize.
 
         The start time is now + the geo optimization duration
         """
@@ -71,3 +97,12 @@ class TourSolverBackend(models.Model):
     def _get_loading_duration_formatted(self):
         h, m = divmod(self.loading_duration, 60)
         return f"{h:02d}:{m:02d}:00"
+
+    def _get_backend_default_options(self):
+        return {"maxOptimDuration": seconds_to_duration(self.duration)}
+
+    def _get_backend_options(self):
+        self.ensure_one()
+        result = self._get_backend_default_options()
+        result.update({p.get("string"): p.get("value") for p in self.backend_options})
+        return result
